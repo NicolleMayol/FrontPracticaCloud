697a68eb7d516167e583ecfbfc7db938a44a1128
NicolleMayol <nmayolm@gmail.com>
2023-10-24 16:12:06 -0600

    Add lambda url

--- src/app/landing/services/user.service.ts
+++ src/app/landing/services/user.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
   getUsers(): Observable<any[]> {
-    const lambdaUrl = 'YOUR_LAMBDA_URL'; // Replace with your Lambda URL
+    const lambdaUrl = 'https://rpoih7c5l2zxwgytu32xyc2w6y0mvcqn.lambda-url.us-east-1.on.aws/'; // Replace with your Lambda URL
     return new Observable<any[]>(observer => {
       fetch(lambdaUrl)
         .then(response => {
